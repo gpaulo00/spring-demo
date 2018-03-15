bd29a86718f96cf9cc4835bc86a1b33887f4bc99
Gustavo Paulo <gustavo.paulo.segura@gmail.com>
2018-03-15 19:04:56 -0400

    fixing code

--- src/main/java/org/gpaulo/springdemo/controller/UserController.java
+++ src/main/java/org/gpaulo/springdemo/controller/UserController.java
@@ -7,53 +7,62 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import models.User;
-import repos.UserRepository;
+import org.gpaulo.springdemo.models.User;
+import org.gpaulo.springdemo.repos.UserRepository;
 
 @RestController
 public class UserController {
+    private UserRepository repository;
+
+    /**
+     * Setup the UserController
+     *
+     * @param userRepository UserRepository impl.
+     */
     @Autowired
-    UserRepository repository;
+    public UserController(UserRepository userRepository) {
+        this.repository = userRepository;
+    }
 
     @RequestMapping("/save")
-    public String process(){
-        // save a single Customer
-        repository.save(new User("Jack", "Smith"));
+    public String process() {
+        // save a single user
+        repository.save(new User("Gustavo", "Paulo"));
 
         // save a list of Customers
-        repository.save(Arrays.asList(new User("Adam", "Johnson"), new User("Kim", "Smith"),
-                new User("David", "Williams"), new User("Peter", "Davis")));
+        repository.saveAll(Arrays.asList(
+                new User("Jhonny", "Paulo"),
+                new User("Estheban", "Paulo"),
+                new User("Rossmy", "Segura")
+        ));
 
         return "Done";
     }
 
-
     @RequestMapping("/findall")
-    public String findAll(){
-        String result = "";
+    public String findAll() {
+        StringBuilder result = new StringBuilder();
 
-        for(User cust : repository.findAll()){
-            result += cust.toString() + "<br>";
+        for (User u : repository.findAll()) {
+            result.append(u.toString()).append("<br>");
         }
 
-        return result;
+        return result.toString();
     }
 
     @RequestMapping("/findbyid")
-    public String findById(@RequestParam("id") long id){
-        String result = "";
-        result = repository.findOne(id).toString();
-        return result;
+    public String findById(@RequestParam("id") long id) {
+        return repository.findById(id).toString();
     }
 
     @RequestMapping("/findbylastname")
-    public String fetchDataByLastName(@RequestParam("lastname") String lastName){
-        String result = "";
+    public String fetchDataByLastName(@RequestParam("lastname") String lastName) {
+        StringBuilder result = new StringBuilder();
 
-        for(User cust: repository.findByLastName(lastName)){
-            result += cust.toString() + "<br>";
+        for (User u : repository.findByLastName(lastName)) {
+            result.append(u.toString()).append("<br>");
         }
 
-        return result;
+        return result.toString();
     }
 }
--- src/main/java/org/gpaulo/springdemo/models/User.java
+++ src/main/java/org/gpaulo/springdemo/models/User.java
@@ -1,5 +1,8 @@
 package org.gpaulo.springdemo.models;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
 @Entity
 @Table(name = "users")
 public class User implements Serializable {
