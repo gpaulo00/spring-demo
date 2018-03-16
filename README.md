811eddad5a22baf91e6dec446c791cfe2d0add6b
Gustavo Paulo <gustavo.paulo.segura@gmail.com>
2018-03-16 00:26:58 -0400

    add README.md

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# Spring Demo
+it is a basic web app, written using **Spring Boot**.
+
+## Build
+Run `mvn package` to build the project, this will create a single file
+in `target/spring-demo-VERSION.jar`. Then you can start the server, using:
+```bash
+java -jar target/spring-demo-VERSION.jar
+```
+
+## Built With
+* [Java](https://www.java.com/download/) - Programming Language
+* [Maven](https://maven.apache.org/download.cgi) - Dependency Management and Build Tool
+* [PostgreSQL](https://www.postgresql.org/) - Relational Database
+
+## Author
+* **Gustavo Paulo** - [gpaulo00](https://github.com/gpaulo00)
--- src/main/java/org/gpaulo/springdemo/models/User.java
+++ src/main/java/org/gpaulo/springdemo/models/User.java
@@ -1,6 +1,7 @@
 package org.gpaulo.springdemo.models;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.io.Serializable;
 
 @Entity
@@ -12,22 +13,35 @@ public class User implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    @Column(name = "firstname")
+    @NotNull
+    @NotEmpty
+    @Column
     private String firstName;
 
-    @Column(name = "lastname")
+    @NotNull
+    @NotEmpty
+    @Column
     private String lastName;
+
+    @Positive
+    @Column
+    private int age;
 
     protected User() {
     }
 
     public User(String firstName, String lastName) {
+        this(firstName, lastName, 1);
+    }
+
+    public User(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.age = age;
     }
 
     @Override
     public String toString() {
-        return String.format("User[id=%d, firstName='%s', lastName='%s']", id, firstName, lastName);
+        return String.format("User[id=%d, firstName='%s', lastName='%s', age='%d']", id, firstName, lastName, age);
     }
 }
