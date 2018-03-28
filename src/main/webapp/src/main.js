a2f4cd7617e603047deae8393b3d008c17fee95d
Gustavo Paulo <gustavo.paulo.segura@gmail.com>
2018-03-28 18:25:16 -0400

    customizing webapp frontend

--- src/main/webapp/src/main.js
+++ src/main/webapp/src/main.js
@@ -1,14 +1,20 @@
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/es5/util/colors'
 
 import App from './App'
 import router from './router'
 
-Vue.use(Vuetify)
-
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.indigo.base,
+    accent: colors.pink.accent2,
+  },
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
