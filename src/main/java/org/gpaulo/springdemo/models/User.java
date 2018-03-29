e0f4a41c8e4054d62c8837d5572c901e54b84518
Gustavo Paulo <gustavo.paulo.segura@gmail.com>
2018-03-29 17:02:04 -0400

    beautify java code

--- src/main/java/org/gpaulo/springdemo/models/User.java
+++ src/main/java/org/gpaulo/springdemo/models/User.java
@@ -1,53 +1,65 @@
 package org.gpaulo.springdemo.models;
 
+import java.io.Serializable;
 import javax.persistence.*;
 import javax.validation.constraints.*;
-import java.io.Serializable;
 
 @Entity
 @Table(name = "users")
 public class User implements Serializable {
-    private static final long serialVersionUID = 1709299723906950395L;
+  private static final long serialVersionUID = 1709299723906950395L;
 
-	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
 
-    @NotNull
-    @NotEmpty
-    @Column
-    private String firstName;
+  @NotNull @NotEmpty @Column private String firstName;
 
-    @NotNull
-    @NotEmpty
-    @Column
-    private String lastName;
+  @NotNull @NotEmpty @Column private String lastName;
 
-    @Positive
-    @Column
-    private int age;
+  @Positive @Column private int age;
 
-    protected User() { }
+  protected User() {}
 
-    public User(String firstName, String lastName) {
-        this(firstName, lastName, 1);
-    }
+  public User(String firstName, String lastName) {
+    this(firstName, lastName, 1);
+  }
 
-    public User(String firstName, String lastName, int age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-    }
+  public User(String firstName, String lastName, int age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
 
-    public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
+  public String getLastName() {
+    return lastName;
+  }
 
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 
-    public int getAge() { return age; }
-    public void setAge(int age) { this.age = age; }
+  public String getFirstName() {
+    return firstName;
+  }
 
-    public long getId() { return id; }
-    public void setId(long id) { this.id = id; }
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
 }
